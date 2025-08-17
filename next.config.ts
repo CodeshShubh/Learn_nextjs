f076772d02ef58c0f2813089363128d50dbbbeda
Akashh Das <akashh@Akashhs-MacBook-Air.local>
2025-08-17 15:48:49 +0530

    hello world

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
-  import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-  const nextConfig: NextConfig = {
-    /* config options here */
-    images:{
-      remotePatterns:[{hostname:'upload.wikimedia.org'}]
-    }
-  };
+const nextConfig: NextConfig = {
+  /* config options here */
+};
 
-  export default nextConfig;
-
+export default nextConfig;

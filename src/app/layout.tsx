56a3d4bece3b82448444bf9ee2f8cf8e0e626847
Akashh Das <codeshshubh@gmail.com>
2025-08-19 03:38:31 +0530

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,13 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
     return (
         <html lang="en">
             <body>
-                <main>
+                <header>
+                    <h1 style={{backgroundColor:'black', color:'white'}}>this is Header</h1>
+                </header>
                     {children}
-                </main>
+               <footer style={{backgroundColor:'black'}}>
+                 <h2 style={{ margin:'0 auto', textAlign:'center', width:'150px', backgroundColor:'black', color:'white'}}>This is footer</h2>
+               </footer>
             </body>
         </html>
     );

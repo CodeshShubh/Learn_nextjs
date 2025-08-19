8879c0ba2763f50487e41c8f28c0b80ed9d1c893
Akashh Das <codeshshubh@gmail.com>
2025-08-19 06:26:31 +0530

    dynamic Metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,6 +1,29 @@
+import { Metadata } from "next";
 
 
-export default async function ProductDetils({params}:{params:Promise<{productId :string}>}) {
+
+type Props = {
+    params: {productId:string}
+}
+
+export async function  generateMetadata({params}:Props):Promise<Metadata>{
+  const {productId} = params;
+
+      const description = await new Promise<string>((resolve)=>{
+         setTimeout(()=>{
+          resolve(`product iphone description`)
+         },1000)
+      })
+
+
+    return {
+        title:`product ${productId}`,
+        description,
+    }
+
+}
+
+export default async function ProductDetils({params}:Props) {
 
       const productId = (await params).productId
     return (

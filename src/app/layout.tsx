054d8dfcc8fc6b31c79a5489e2a1b0fb07e67b7c
Akashh Das <codeshshubh@gmail.com>
2025-08-19 07:37:11 +0530

    Link tag stying or active Links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,48 @@
-import { Metadata } from "next";
+'use client';
+// import { Metadata } from "next";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-export const metadata:Metadata = {
-    title:'Home',
-    description:'Home Description'
-}
+// export const metadata:Metadata = {
+//     title:'Home',
+//     description:'Home Description'
+// }
+
+const navLinks  = [
+    {name:"forgot-password" , href:'/forgot-password'},
+    {name:'login', href:'/login'},
+    {name:'register', href:'/register'},
+    {name:'about', href:'/about'},
+    {name:'contact', href:'/contact'},
+    {name:'products', href:'/products'},
+]
+
 export default function registerLayout({children}: {children: React.ReactNode}) {
+     const pathName = usePathname()
+      console.log(pathName)
+      
     return (
         <html lang="en">
             <body>
-                <main>
-
+                     {
+                         navLinks.map((itm,i)=>{
+                              const isActive = pathName === itm.href ||(pathName.startsWith(itm.href) && itm.href!=='/')
+                            return (
+                                    
+                                 <div key={i} >
+                                      <Link href={itm.href} style={isActive ? {color:"red"}: {color:'blue'}}>{itm.name}</Link>
+                                 </div>
+                            )
+                         })
+                     }
                     {children}
-                    
-                </main>
                 
             </body>
         </html>
     );
 }
+
+
+
+
+

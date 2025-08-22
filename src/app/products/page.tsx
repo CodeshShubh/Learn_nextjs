4cf9822e8159c0ebf8d03849e7121204dadb8387
Akashh Das <codeshshubh@gmail.com>
2025-08-22 17:59:37 +0530

    loading.tsx

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,9 +4,16 @@ export const metadata:Metadata = {
     title:'products Page',
     description:'Product description'
 }
-export default function Products() {
+export default async function Products() {
+
+     await new Promise((resolve)=>{
+         setTimeout(()=>{
+       resolve('intentially delay')
+         },2000)
+     })
     return (
         <div>
+            
             <h1>page List</h1>
             <p> where user click on product so redirect on that perticuler product</p>
         </div>

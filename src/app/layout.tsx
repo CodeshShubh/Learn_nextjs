e35de9ca2bb2762730b958e7412e870371ba1c12
Akashh Das <codeshshubh@gmail.com>
2025-08-22 16:15:32 +0530

    active links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,30 @@
-'use client';
-// import { Metadata } from "next";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+// 'use client';
+// // import { Metadata } from "next";
+// import Link from "next/link";
+// import { usePathname } from "next/navigation";
 
 // export const metadata:Metadata = {
 //     title:'Home',
 //     description:'Home Description'
 // }
 
-const navLinks  = [
-    {name:"forgot-password" , href:'/forgot-password'},
-    {name:'login', href:'/login'},
-    {name:'register', href:'/register'},
-    {name:'about', href:'/about'},
-    {name:'contact', href:'/contact'},
-    {name:'products', href:'/products'},
-]
+// const navLinks  = [
+//     {name:"forgot-password" , href:'/forgot-password'},
+//     {name:'login', href:'/login'},
+//     {name:'register', href:'/register'},
+//     {name:'about', href:'/about'},
+//     {name:'contact', href:'/contact'},
+//     {name:'products', href:'/products'},
+// ]
 
 export default function registerLayout({children}: {children: React.ReactNode}) {
-     const pathName = usePathname()
-      console.log(pathName)
+    //  const pathName = usePathname()
+    //   console.log(pathName)
       
     return (
         <html lang="en">
             <body>
-                     {
+                     {/* {
                          navLinks.map((itm,i)=>{
                               const isActive = pathName === itm.href ||(pathName.startsWith(itm.href) && itm.href!=='/')
                             return (
@@ -34,7 +34,7 @@ export default function registerLayout({children}: {children: React.ReactNode}) 
                                  </div>
                             )
                          })
-                     }
+                     } */}
                     {children}
                 
             </body>
